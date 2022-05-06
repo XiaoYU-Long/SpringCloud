ecb3cea948e90648bbb27a859b964eb3533a1214
HongYu <594720426@qq.com>
2022-05-06 17:48:55 +0800

    eureka服务注册相关代码提交

---
+++ service-provider/src/test/java/com/lhy/serviceprovider/ServiceProviderApplicationTests.java
@@ -0,0 +1,14 @@
+package com.lhy.serviceprovider;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceProviderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
