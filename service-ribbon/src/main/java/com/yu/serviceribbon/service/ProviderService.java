75bdf2d5945ad4c749209360271af92036c8a46e
HongYu <594720426@qq.com>
2022-05-11 20:37:53 +0800

    ribbon服务添加hystrix熔断器

--- service-ribbon/src/main/java/com/yu/serviceribbon/service/ProviderService.java
+++ service-ribbon/src/main/java/com/yu/serviceribbon/service/ProviderService.java
@@ -1,5 +1,6 @@
 package com.yu.serviceribbon.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -16,7 +17,12 @@ public class ProviderService {
     @Autowired
     private RestTemplate restTemplate;
 
+    @HystrixCommand(fallbackMethod = "fallback")
     public String hello(String name) {
         return restTemplate.getForObject("http://SERVICE-PROVIDER/hello?name=" + name, String.class);
+    }
+
+    public String fallback(String name) {
+        return "hello," + name + ",sorry, the service is down!";
     }
 }
